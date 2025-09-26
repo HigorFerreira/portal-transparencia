a6b87a01624c593d0c4712a93f4aacd8040c85a3
Higor <hfashigor@gmail.com>
2025-09-26 18:17:21 -0300

    Adjust

--- src/modules/server/TopSecondBar/Container.tsx
+++ src/modules/server/TopSecondBar/Container.tsx
@@ -1,10 +1,32 @@
 'use client';
 
-import { ReactNode, useCallback, useEffect, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction, useCallback, useEffect, useState } from "react";
+
+function useTransitionLocker(){
+    const [ isTransitioning, setIsTransitioning ] = useState(false)
+
+    const transitionStart = () => setIsTransitioning(true)
+    const transitionEnd = () => setIsTransitioning(false)
+
+    return [ isTransitioning, { transitionStart, transitionEnd } ] as const
+}
+
+function useStateWithLocker<T>(initialState: T, locker: boolean){
+    const [ state, setState ] = useState<T>(initialState)
+
+    const setter: Dispatch<SetStateAction<T>> = arg => {
+        if(!locker){
+            setState(arg)
+        }
+    }
+
+    return [ state, setter ] as const
+}
 
 export default function Container({ children }: { children?: ReactNode }){
 
-    const [ scrolled, setScrolled ] = useState(false)
+    const [ isTransitioning, { transitionStart, transitionEnd } ] = useTransitionLocker()
+    const [ scrolled, setScrolled ] = useStateWithLocker(false, isTransitioning)
 
     const scrollFn = useCallback(() => {
         if(window.scrollY >= 50){
@@ -13,7 +35,7 @@ export default function Container({ children }: { children?: ReactNode }){
         else {
             setScrolled(false)
         }
-    }, [])
+    }, [ setScrolled ])
 
     useEffect(() => {
         window.addEventListener('scroll', scrollFn)
@@ -29,6 +51,8 @@ export default function Container({ children }: { children?: ReactNode }){
             scrolled ? "scrolled" : "",
             scrolled ? "h-[45px]" : "h-[80px]"
         ].join(" ")}
+        onTransitionStart={transitionStart}
+        onTransitionEnd={transitionEnd}
     >
         { children }
     </div>
