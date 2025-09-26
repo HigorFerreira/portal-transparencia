fe4bcdd2baa8de88f67f67924362e5b24681298d
Higor <hfashigor@gmail.com>
2025-09-26 17:24:24 -0300

    Top bars

---
+++ src/modules/server/TopSecondBar/Container.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ReactNode, useCallback, useEffect, useState } from "react";
+
+export default function Container({ children }: { children?: ReactNode }){
+
+    const [ scrolled, setScrolled ] = useState(false)
+
+    const scrollFn = useCallback(() => {
+        if(window.scrollY > 50){
+            setScrolled(true)
+        }
+        else {
+            setScrolled(false)
+        }
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrollFn)
+        return () => window.removeEventListener('scroll', scrollFn)
+    }, [ scrollFn ])
+
+    // transition: all 400ms ease;
+    return <div
+        className={[
+            "@container/top-second-bar",
+            "sticky top-[40px] left-0 right-0 shadow flex items-center justify-center bg-white",
+            "transition-all",
+            scrolled ? "scrolled" : "",
+            scrolled ? "h-[45px]" : "h-[80px]"
+        ].join(" ")}
+    >
+        { children }
+    </div>
+}
---
+++ src/modules/server/TopSecondBar/index.tsx
@@ -0,0 +1,33 @@
+import TJGOLogo from '../TJGOLogo'
+import { IoMenuOutline as MenuIcon } from "react-icons/io5";
+import Container from './Container';
+
+export async function TopSecondBar(){
+    return <Container>
+        <div
+            className={[
+                'flex justify-between items-center w-full px-6',
+                '[@container_top-second-bar_(width>=600px)]:justify-center',
+                '[@container_top-second-bar_(width>=600px)]:gap-6',
+
+                '[&_#g281]:transition-all',
+                '[&_#g315]:transition-all',
+
+                '[.scrolled_&_#g281]:scale-50',
+                '[.scrolled_&_#g281]:translate-y-[calc(50%-29.8px/2)]',
+                '[.scrolled_&_#g281]:translate-x-[calc(22.04px*1.5)]',
+
+                '[.scrolled_&_#g315]:scale-[0.9]',
+                '[.scrolled_&_#g315]:translate-y-[calc(2px)]',
+                '[.scrolled_&_#g315]:translate-x-[calc(5px)]',
+            ].join(' ')}
+        >
+            <div>
+                <MenuIcon />
+            </div>
+            <div className='[&_svg]:h-[60px]'>
+                <TJGOLogo />
+            </div>
+        </div>
+    </Container>
+}
