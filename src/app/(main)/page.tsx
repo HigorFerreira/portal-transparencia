7048498c4ac1befaccba9deb1eb4a79b798c533c
Higor <hfashigor@gmail.com>
2025-09-30 19:44:48 -0300

    Containers

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,15 @@
 
 
 export default function Home() {
-	return <div>
-		<div className="w-full h-[calc(100dvh-30px-40px-80px)]">
+	return <div className="snap-y snap-proximity">
+		<div className="w-full h-[calc(100dvh-30px-40px-80px)] snap-center">
 			<div className="absolute left-0 bg-gray-400/10 w-full h-[calc(100dvh-30px-40px-80px)]"></div>
 		</div>
-		<div className="w-full h-[calc(100dvh-30px-40px-80px)]">
+		<div className="w-full h-[calc(100dvh-30px-40px-80px)] snap-center">
 			<div className="absolute left-0 bg-white w-full h-[calc(100dvh)]"></div>
+		</div>
+		<div className="w-full h-[calc(100dvh-30px-40px-80px)] snap-center">
+			<div className="absolute left-0 bg-gray-400/10 w-full h-[calc(100dvh)]"></div>
 		</div>
 	</div>
 }
