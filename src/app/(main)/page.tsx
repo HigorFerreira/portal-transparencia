3df289002009edc346d3c2654d1ef860a07cc4cc
Higor <hfashigor@gmail.com>
2025-09-30 19:35:37 -0300

    Testing containers

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,12 @@
-import ClientComponent from './ClientTest'
+
 
 export default function Home() {
 	return <div>
-		{/* <TopBar />
-		<TopSecondBar /> */}
-		<div className='pt-[80px] h-[200vh]'>
-			<ClientComponent />
+		<div className="w-full h-[calc(100dvh-30px-40px-80px)]">
+			<div className="absolute left-0 bg-gray-400/10 w-full h-[calc(100dvh-30px-40px-80px)]"></div>
+		</div>
+		<div className="w-full h-[calc(100dvh-30px-40px-80px)]">
+			<div className="absolute left-0 bg-white w-full h-[calc(100dvh)]"></div>
 		</div>
 	</div>
 }
