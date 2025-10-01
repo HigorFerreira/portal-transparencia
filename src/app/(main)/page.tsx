e423a61d4743e079906e62f99aa7122b8f20b6b3
Higor <hfashigor@gmail.com>
2025-10-01 17:36:22 -0300

    First disposition

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,22 +6,23 @@ export default function Home() {
 			"w-[100vw] h-[100dvh] grid",
 			"grid-cols-[auto_auto_1fr] grid-rows-[40px_auto_1fr]",
 			"[grid-template-areas:'h1_h1_h1'_'mm_sm_h2'_'mm_sm_c']",
+			"[&>*]:flex [&>*]:justify-center [&>*]:items-center",
 		].join(" ")}
 	>
 		<header className="[grid-area:h1]">
-
+			HEADER 1
 		</header>
 		<header className="[grid-area:h2]">
-
+			HEADER 2
 		</header>
 		<aside className="[grid-area:mm]">
-
+			MAIN MENU
 		</aside>
 		<aside className="[grid-area:sm]">
-
+			SUBMENU
 		</aside>
 		<main className="[grid-area:c]">
-
+			CONTENT
 		</main>
 	</div>
 }
