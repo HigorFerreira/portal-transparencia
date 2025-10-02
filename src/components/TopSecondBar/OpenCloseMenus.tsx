5388e2322184abe6eeefd4c0d4e1dd567c1eecb5
Higor <hfashigor@hotmail.com>
2025-10-02 08:04:02 -0300

    Testing OpenCloseMenus

--- src/components/TopSecondBar/OpenCloseMenus.tsx
+++ src/components/TopSecondBar/OpenCloseMenus.tsx
@@ -2,21 +2,36 @@
 
 import { TbLayoutSidebarLeftCollapse as CollapseIcon } from "react-icons/tb"
 import { TbLayoutSidebarRightCollapse as ExpandIcon } from "react-icons/tb"
-import { useSubMenu } from "@/components/SideMenuProvider/hooks"
+import { useMenu, useMenuOpen, useSubMenu, useSubMenuOpen } from "@/components/SideMenuProvider/hooks"
+import { useCallback } from "react"
 
 export default function OpenCloseMenus(){
-    const { open, close } = useSubMenu()
+
+    const isMenuOpen = useMenuOpen()
+    const isSubMenuOpen = useSubMenuOpen()
+
+    const { open: openMenu, close: closeMenu } = useMenu()
+    const { open: openSubMenu, close: closeSubMenu } = useSubMenu()
+
+    const handleClose =  useCallback(() => {
+        if(isMenuOpen && isSubMenuOpen){
+            closeSubMenu()
+        }
+        if(isMenuOpen && !isSubMenuOpen){
+            closeMenu()
+        }
+    },  [ isMenuOpen, isSubMenuOpen, closeSubMenu, closeMenu ])
+
     return <div className='absolute left-[calc(var(--spacing)*4)]'>
         <div className='flex gap-1 text-gray-500'>
             <div
-                onClick={close}
+                onClick={handleClose}
                 className='cursor-pointer'
                 title='Fechar menu'
             >
                 <CollapseIcon size={30} />
             </div>
             <div
-                onClick={open}
                 className='cursor-pointer'
                 title='Abrir menu'
             >
