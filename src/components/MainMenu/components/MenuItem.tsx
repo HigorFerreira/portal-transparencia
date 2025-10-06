6d560f78d3242ac7e1c7334571b975f6d76e10b7
Higor <hfashigor@gmail.com>
2025-10-06 20:16:40 -0300

    Small adjust

--- src/components/MainMenu/components/MenuItem.tsx
+++ src/components/MainMenu/components/MenuItem.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren, JSX } from "react"
 
 export default function MenuItem({ children, onClick }: PropsWithChildren<{ onClick?: JSX.IntrinsicElements['p']['onClick'] }>){
-    return <p className="text-(--alt-color) bg-(--menu-item-bg) rounded-xl p-2" onClick={onClick}>
+    return <p title={children as string} className="text-(--alt-color) bg-(--menu-item-bg) rounded-xl p-2 cursor-pointer" onClick={onClick}>
         { children }
     </p>
 }
