a649c7e3cb91e726c06dbda1a4958041dfa5186a
Higor <hfashigor@gmail.com>
2025-10-07 18:23:27 -0300

    Small adjust

--- src/components/TopSecondBar/index.tsx
+++ src/components/TopSecondBar/index.tsx
@@ -6,7 +6,10 @@ export default async function TopSecondBar(){
     return <div className='relative w-full h-full flex justify-center items-center p-4 shadow'>
         <OpenCloseMenus />
         <div className='self-center [&_svg]:h-[60px]'>
-            <TJGOLogo />
+            <div className="w-full flex gap-6">
+                <h1 className="font-bold text-[clamp(1.5rem,calc(.7886vw+1.25rem),2.6rem)] mb-4 text-[#487eb0]">Portal da Transparência</h1>
+                <TJGOLogo />
+            </div>
         </div>
     </div>
 }
