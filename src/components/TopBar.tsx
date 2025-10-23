1fcc5914f211bcc7585e09e10b4920d2a6a5d3c2
Higor <hfashigor@gmail.com>
2025-10-23 16:58:54 -0300

    Adjust

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import { CustomIcons } from './CustomIcons'
+// import { CustomIcons } from './CustomIcons'
 import {
     IoAccessibility as AccessibilityIcon,
     IoContrastSharp as ContrastIcon
@@ -6,14 +6,14 @@ import {
 
 export default async function TopBar(){
 
-    const { GoiasFlag } = await CustomIcons()
+    // const { GoiasFlag } = await CustomIcons()
 
     return <div className="@container/top-bar w-full h-full bg-blue-950 z-1000 px-6">
         <div className={[
             "w-full h-full flex justify-between items-center"
         ].join(' ')}>
             <div>
-                <GoiasFlag height={20} />
+                {/* <GoiasFlag height={20} /> */}
             </div>
             <div className='text-white flex items-center gap-4'>
                 <div title='Contraste' className='cursor-pointer'>
