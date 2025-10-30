bf35fe138370f70eb97426324bab93f7eded0014
Higor <hfashigor@gmail.com>
2025-10-30 14:46:20 -0300

    Detail Adjustment

--- src/components/TopSecondBar/index.tsx
+++ src/components/TopSecondBar/index.tsx
@@ -27,9 +27,12 @@ function useScreenDimensions() {
 function useColors() {
     const colors = useMemo(() => {
         const primary = '#58abe3ff'
-        
+        const primaryDark = '#5496d7ff'
+        const primaryLight = '#b5e7faff'
         return {
-            primary
+            primary,
+            primaryDark,
+            primaryLight,
         } as const
     }, [])
 
@@ -78,7 +81,9 @@ export default function TopSecondBar(){
             container={container}
             onContext={(context, { width, height }, colors) => {
                 const {
-                    primary
+                    primary,
+                    primaryDark,
+                    primaryLight,
                 } = colors;
 
                 context.fillStyle = primary
@@ -90,6 +95,23 @@ export default function TopSecondBar(){
                 context.bezierCurveTo(0.707893011918*width, 58.2031, 0.711957284662*width, 45.2328, 0.711957284662*width, 30.9102)
                 context.bezierCurveTo(0.711957284662*width, 19.3382, 0.709300586970*width, 8.6493, 0.704811664573*width, 0)
                 // 960.20314 + 0b0000, 83.025392 + 0b0000,
+                context.fill()
+
+                context.fillStyle = primaryDark
+                context.beginPath()
+                context.moveTo(139.32637,14.430614)
+                context.lineTo(0, 14.430614)
+                context.lineTo(0, 83.023801)
+                context.lineTo(204.34223, 83.023801)
+                context.bezierCurveTo(204.4369, 43.581219, 175.2863, 14.430614, 139.32637, 14.430614)
+                context.fill()
+
+                context.fillStyle = primaryLight
+                context.beginPath()
+                context.moveTo(139.32637, 144.653)
+                context.bezierCurveTo(174.11697, 144.653, 202.5329, 117.36506, 204.34223, 83.0238)
+                context.lineTo(0, 83.0238)
+                context.lineTo(0, 144.653)
                 context.fill()
             }}
         />
